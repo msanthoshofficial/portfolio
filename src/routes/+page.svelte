9c51574f04dec84bf33c64c693b42838ef17a4b7
Santhosh M <pmsanthosh2000@gmail.com>
2023-07-21 20:29:22 +0530

    portfolio init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center pt-[12rem]">
+		
+		<div class="grid grid-cols-1 gap-4">
+			<div class="grid grid-cols-3 gap-4 h-fit items-center">
+				<div class="text-start col-span-2">
+					<h1 class="h1 text-5xl font-bold">Hey there! I'm Santhosh</h1>
+					<p class="h2">A passionate and dedicated Full-Stack Software Engineer</p>
+				</div>
+				<img src="src\lib\images\engineer.png" alt="">
+			</div>
+		
+	</div>
+</div>
