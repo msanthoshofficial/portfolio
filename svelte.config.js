c1d8b2b35c35b27b494eb7e4cd0bca703e9a04c0
Santhosh M <pmsanthosh2000@gmail.com>
2023-07-22 14:57:31 +0530

    yml init

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,7 +15,7 @@ const config = {
 	
 	kit: {
 		paths:{
-			base:"/portfolio"
+			base: dev ? '' : "/portfolio"
 		},
         adapter: adapter({
             // default options are shown. On some platforms
